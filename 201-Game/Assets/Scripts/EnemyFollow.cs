b9e41947d0edcedb22f161e2bca5a35708938e98
Braedan <270336855@yoobeestudent.ac.nz>
2024-09-05 16:02:45 +1200

    enemies spawn faster over time, shoot projectiles now

--- 201-Game/Assets/Scripts/EnemyFollow.cs
+++ 201-Game/Assets/Scripts/EnemyFollow.cs
@@ -7,15 +7,25 @@ public class EnemyFollow : MonoBehaviour
 {
     public NavMeshAgent enemy;
     public Transform player;
+    public GameObject projectilePrefab;
+    private float startDelay = 1;
+    private float spawnInterval = 5f;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player").transform;
+        InvokeRepeating(nameof(fire), startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {       
         enemy.SetDestination(player.position);
+    }
+
+    void fire()
+    {
+       
+        Instantiate(projectilePrefab, transform.position, transform.rotation);
     }
 }
--- 201-Game/Assets/Scripts/SpawnManager.cs
+++ 201-Game/Assets/Scripts/SpawnManager.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] enemyPrefabs;
-    private float spawnRangeX = 10;
-    private float spawnPosZ = 20;
-    private float startDelay = 2;
-    private float spawnInterval = 1.5f;
+    //for game difficulty enemy spawn rate increases over time
+    public float initialSpawnInt = 6f;//the start rate
+    public float intervalDecrease = 0.025f; // interval decreases by this value after each spawn
+    public float lowestSpawnInt = 0.5f; //lowest rate the interval gets to.
+    private float spawnRangeX = 30;
+    private float spawnPosZ = 35;
+    private float spawnInterval = 4f;
+    private float spawnRangeX2 = -30;
+    private float spawnPosZ2 = -35;   
+    private float currentSpawnInterval;
+
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomEnemy", startDelay, spawnInterval);      
+        currentSpawnInterval = initialSpawnInt;
+        StartCoroutine(SpawnEnemies());
+    }
+    IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            InvokeRepeating(nameof(SpawnRandomEnemy), 0, spawnInterval);
+            InvokeRepeating(nameof(SpawnRandomEnemy2), 0, spawnInterval);
+            yield return new WaitForSeconds(currentSpawnInterval);
+
+            
+            currentSpawnInterval = Mathf.Max(lowestSpawnInt, currentSpawnInterval - intervalDecrease);
+        }
     }
 
     // Update is called once per frame
@@ -23,6 +43,15 @@ public class SpawnManager : MonoBehaviour
    void SpawnRandomEnemy()
     {
         Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 1, spawnPosZ);
+
+        int enemyIndex = Random.Range(0, enemyPrefabs.Length);
+        Instantiate(enemyPrefabs[enemyIndex], spawnPos,
+            enemyPrefabs[enemyIndex].transform.rotation);
+    }
+
+    void SpawnRandomEnemy2()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX2, spawnRangeX2), 1, spawnPosZ2);
 
         int enemyIndex = Random.Range(0, enemyPrefabs.Length);
         Instantiate(enemyPrefabs[enemyIndex], spawnPos,
